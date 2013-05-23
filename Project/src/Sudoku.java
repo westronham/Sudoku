e436717c9e2d287501ac233ca1a5804e5956db7c
Ron <sacksackley@gmail.com>
2013-05-23 16:27:29 +1000

    Ver 1.0

--- Project/src/Sudoku.java
+++ Project/src/Sudoku.java
@@ -1,125 +1,163 @@
-//We'll have two doubly linked lists
-//The rows of the matrix are doubly linked as circular lists via the L and R fields
-//Columns are doubly linked as circular lists via the U and D fields. 
-//Each column list also includes a special data object called its list header
+/*
+ * Ideas of what to do next?
+ * 1) We should make a 3rd Sudoku array for user input
+ * 		That way if we want to check the solution it's easy, we just compare it to our Sudoku solutions
+ * 		Every time the user types in a number in a cell in our GUI, it should write to the corresponding cell in the userSudoku 2d array
+ * 2) Haven't gotten a generate sudoku algorithm. At the moment I'm thinking maybe we just focus on getting our GUI
+ * 		and hints all done and if we have time we can do the generate sudoku algorithm.
+ * 		We can make a folder for easy, medium, hard and have a heap of Sudoku txt files that the user can choose from at the start
+ * 		A lot of Sudoku apps do this and if they ask us about why we chose to do this we can say it's because we can provide
+ * 		updates to the user with more puzzles and also allows us greater control over things such as difficulty of puzzle that a user gets
+ * 3) I'm going to read into saving, and also I'm going to try get the Sudoku grid into a Sudoku GUI rather than have it print the numbers like it does
+ */
 public class Sudoku {
 	public Sudoku () {
 		sudoku = new int[9][9];
+		unsolvedSudoku = new int[9][9];
 	}
 	
+	/*
+	 * We need to initialise a few things in our Sudoku before we can solve it
+	 * Firstly we need to load all the given values into the correct coordinates
+	 * Then we just initialise a few values:
+	 * gridSize - will always be 9
+	 * gridBoxSize - will always be 3
+	 * I'm trying to make a copy of the unsolved Sudoku so we can reload it if the user wants to restart but it's not working yet
+	 */
 	public void initSudoku(int[] coordinates) {
 		int row = 0;
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
-			sudoku[j][i] = coordinates[j + (9 * row)];
+			unsolvedSudoku[j][i] = coordinates[j + (9 * row)];
 			}
 			row++;
 		}
 		gridSize = sudoku.length;
 		gridBoxSize = (int)Math.sqrt(gridSize);
-		unsolvedSudoku = (int[][])sudoku.clone();
+		System.arraycopy(unsolvedSudoku, 0, sudoku, 0, gridSize);
 	}
 	
-	public int[][] getUnsolvedSudoku() {
-		return sudoku;
-	}
-	
-	public void printUnsolvedSudoku() {
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
+	/*
+	 * Simply prints the Sudoku
+	 */
+	public void printSudoku() {
+		for (int i = 0; i < gridSize; i++) {
+			for (int j = 0; j < gridSize; j++) {
 				System.out.print(sudoku[j][i] + " ");
 			}
 			System.out.println();
 		}
 	}
-		
-	 /*
-	  * There are 3 different conditions we need to meet in order for a number to be valid
-	  * 	1) A number must only appear once in any row
-	  * 	2) A number must only appear once in any column
-	  * 	3) A number must only appear once in all 3x3 grids
-	  * We create 3 9x9 grids that hold true in a cell corresponding to the row/column/box of a value in the actual Sudoku if it satisfies the condition
-	  * When we initialise them we set the flags for all values that aren't 0 (i.e. for actual Sudoku entries)
-	  */
-	 public void initConditionMatrices() {
-	        rowMatrix = new boolean[gridSize][gridSize];
-	        colMatrix = new boolean[gridSize][gridSize];
-	        boxMatrix = new boolean[gridSize][gridSize];
-	        for(int i = 0; i < sudoku.length; i++) {
-	            for(int j = 0; j < sudoku.length; j++) {
-	                int value = sudoku[i][j];
-	                if(value != 0) {
-	                    setValueConditions(i, j, value, true);
-	                }
-	            }
+	
+ /*
+  * There are 3 different conditions we need to meet in order for a number to be valid
+  * 	1) A number must only appear once in any row
+  * 	2) A number must only appear once in any column
+  * 	3) A number must only appear once in all 3x3 grids
+  * We create 3 9x9 grids that hold true in a cell corresponding to the row/column/box of a value in the actual Sudoku if it satisfies the condition
+  * When we initialise them we set the flags for all values that aren't 0 (i.e. for actual Sudoku entries)
+  * It allows us to really quickly check if there is already a number in a row/column/box before we put it in a cell
+  */
+	public void initConditionMatrices() {
+        rowMatrix = new boolean[gridSize][gridSize];
+        colMatrix = new boolean[gridSize][gridSize];
+        boxMatrix = new boolean[gridSize][gridSize];
+        for(int i = 0; i < sudoku.length; i++) {
+            for(int j = 0; j < sudoku.length; j++) {
+                int value = sudoku[i][j];
+                if(value != 0) {
+                    setValueConditions(i, j, value, true);
+                }
+            }
+        }
+    }
+ 
+ /*
+  * We use this method to flag different values as satisfying the specific constraint
+  * rowSubset - We go to row i and put down that our value is present in that row
+  * solSubset - We go to column j and put down that our value is present in that column
+  * boxSubset - We determine which box our coordinates place us in and then put down that our value is present in that box
+  * Note how we do value-1 because our index starts from 0
+  */
+	private void setValueConditions(int i, int j, int value, boolean exists) {
+	    rowMatrix[i][value - 1] = exists;
+	    colMatrix[j][value - 1] = exists;
+	    boxMatrix[findBoxNum(i, j)][value - 1] = exists;
+	}
+
+/*
+ * Our solve algorithm is given the coordinates from which to solve onwards
+ * This method simply calls it to start from the first cell and thus solve the whole Sudoku
+ */
+public boolean solve() {
+    return solve(0, 0);
+}
+
+/*
+ * The algorithm that actually figures out what number goes where in our Sudoku
+ * I'll break it up:
+ * The algorithm takes in 2 coordinates: i is the column and j is the row number and from these points onwards we get the solutions
+ * Firstly this is a recursive algorithm so we have a base case which is if we get to the last coordinate in the grid
+ * Otherwise we go through each coordinate
+ * 		If the value in the coordinate is not 0 (i.e. a given value), we recurse to the next cell
+ * 		Otherwise we want to figure out the value of the cell so we go through every possible value and if it is a valid value
+ * 		we put the value in that cell and flag it in the conditional matrices
+ * 		Now the backtracking comes into play (it took me ages to wrap my head around this so message me if it confuses you):
+ * 			If we recurse forward without encountering any issues, we return all true and we have the right answer
+ * 			If the number turns out to be bad we change the value back to 0 and change the conditional matrix back to false and try the next available value
+ */
+	public boolean solve(int i, int j) {
+	    if(i == gridSize) {
+	        i = 0;
+	        if(++j == gridSize) {
+	            return true;
 	        }
 	    }
-	 
-	 /*
-	  * We use this method to flag different values as satisfying the specific constraint
-	  * rowSubset - We go to row i and put down that our value is present in that row
-	  * solSubset - We go to column j and put down that our value is present in that column
-	  * boxSubset - We determine which box our coordinates place us in and then put down that our value is present in that box
-	  * Note how we do value-1 because our index starts from 0
-	  */
-	    private void setValueConditions(int i, int j, int value, boolean exists) {
-	        rowMatrix[i][value - 1] = exists;
-	        colMatrix[j][value - 1] = exists;
-	        boxMatrix[findBoxNum(i, j)][value - 1] = exists;
+	    if(sudoku[i][j] != 0) {
+	        return solve(i + 1, j);
 	    }
-	    
-	    /*
-	     * To solve for every value we start from 0
-	     */
-	    public boolean solve() {
-	        return solve(0, 0);
-	    }
-	    
-	    
-	    public boolean solve(int i, int j) {
-	        if(i == gridSize) {
-	            i = 0;
-	            if(++j == gridSize) {
+	    for(int value = 1; value <= gridSize; value++) {
+	        if(isValid(i, j, value)) {
+	            sudoku[i][j] = value;
+	            setValueConditions(i, j, value, true);
+	            if(solve(i + 1, j)) {
 	                return true;
 	            }
+	            setValueConditions(i, j, value, false);
 	        }
-	        if(sudoku[i][j] != 0) {
-	            return solve(i + 1, j);
-	        }
-	        for(int value = 1; value <= gridSize; value++) {
-	            if(isValid(i, j, value)) {
-	                sudoku[i][j] = value;
-	                setValueConditions(i, j, value, true);
-	                if(solve(i + 1, j)) {
-	                    return true;
-	                }
-	                setValueConditions(i, j, value, false);
-	            }
-	        }
-	 
-	        sudoku[i][j] = 0;
-	        return false;
 	    }
-	    
-	    /*
-	     * Method that checks whether a number in a certain location is valid
-	     * It does this by checking our row, column and box conditional matrices
-	     * If none of the conditions have been satisfied already it returns false and so we return !false (i.e. true)
-	     */
-	    private boolean isValid(int i, int j, int value) {
-	        value--;
-	        boolean present = rowMatrix[i][value] || colMatrix[j][value] || boxMatrix[findBoxNum(i, j)][value];
-	        return !present;
-	    }
-	    
-	    /*
-	     * Quick method for calculating which box any coordinate is in
-	     */
-	    private int findBoxNum(int i, int j) {
-	        int boxRow = i / gridBoxSize;
-	        int boxCol = j / gridBoxSize;
-	        return (boxRow * gridBoxSize) + boxCol;
-	    }
+	    sudoku[i][j] = 0;
+	    return false;
+	}
+
+/*
+ * Method that checks whether a number in a certain location is valid
+ * It does this by checking our row, column and box conditional matrices
+ * If none of the conditions have been satisfied already it returns false and so we return !false (i.e. true)
+ */
+	private boolean isValid(int i, int j, int value) {
+	    value--;
+	    boolean present = rowMatrix[i][value] || colMatrix[j][value] || boxMatrix[findBoxNum(i, j)][value];
+	    return !present;
+	}
+
+/*
+ * Quick method for calculating which box any coordinate is in
+ */
+	private int findBoxNum(int i, int j) {
+	    int boxRow = i / gridBoxSize;
+	    int boxCol = j / gridBoxSize;
+	    return (boxRow * gridBoxSize) + boxCol;
+	}
+	
+	/*
+	 * NOT WORKING YET
+	 * Pretty self-explanatory
+	 * If we want to start our sudoku again we just reload it with the default values
+	 */
+	public void resetSudoku() {
+		System.arraycopy(unsolvedSudoku, 0, sudoku, 0, gridSize);
+	}
 	
 	
 	private int[][] sudoku;
--- Project/src/SudokuTest2.java
+++ Project/src/SudokuTest2.java
@@ -10,18 +10,18 @@ public class SudokuTest2 {
 	public static void main(String[] args) {
 		try {
 		Scanner scanner = new Scanner(new FileReader(args[0]));
-		int [] tall = new int [100];
+		int[] row = new int [100];
 		int i = 0;
 		while(scanner.hasNextInt()){
-		   tall[i++] = scanner.nextInt();
+		   row[i++] = scanner.nextInt();
 		}
 		scanner.close();
 		
 		Sudoku sudoku = new Sudoku();
-		sudoku.initSudoku(tall);
+		sudoku.initSudoku(row);
 		sudoku.initConditionMatrices();
 		sudoku.solve();
-		sudoku.printUnsolvedSudoku();
+		sudoku.printSudoku();
 		Client c = new Client();
 		} catch (FileNotFoundException e) {}
 
