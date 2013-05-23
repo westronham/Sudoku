da2043155559379cb1e610f7c5046ca0adebdf7a
Ron <sacksackley@gmail.com>
2013-05-23 16:28:34 +1000

    Ver 1.0

--- Project/src/SudokuTest2.java
+++ Project/src/SudokuTest2.java
@@ -9,20 +9,20 @@ public class SudokuTest2 {
 	 */
 	public static void main(String[] args) {
 		try {
-		Scanner scanner = new Scanner(new FileReader(args[0]));
-		int[] row = new int [100];
-		int i = 0;
-		while(scanner.hasNextInt()){
-		   row[i++] = scanner.nextInt();
-		}
-		scanner.close();
+			Scanner scanner = new Scanner(new FileReader(args[0]));
+			int[] row = new int [100];
+			int i = 0;
+			while(scanner.hasNextInt()){
+			   row[i++] = scanner.nextInt();
+			}
+			scanner.close();
 		
-		Sudoku sudoku = new Sudoku();
-		sudoku.initSudoku(row);
-		sudoku.initConditionMatrices();
-		sudoku.solve();
-		sudoku.printSudoku();
-		Client c = new Client();
+			Sudoku sudoku = new Sudoku();
+			sudoku.initSudoku(row);
+			sudoku.initConditionMatrices();
+			sudoku.solve();
+			sudoku.printSudoku();
+			Client c = new Client();
 		} catch (FileNotFoundException e) {}
 
 		
