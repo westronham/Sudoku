213e0ac0a09cfe77551befa8c418b511c87de5a9
westronham <sacksackley@gmail.com>
2013-05-28 12:11:48 +1000

    Ver 1.5

--- Project/src/PlayMenu.java
+++ Project/src/PlayMenu.java
@@ -1,6 +1,9 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.text.AbstractDocument;
+import javax.swing.text.Document;
+
 
 /*TODO: We need to restrict input to jTextArea to numbers only
  *"Finish and Check Answer" should ask sudoku if it is solved correctly, if not 
@@ -8,29 +11,24 @@ import javax.swing.*;
  * When 
  * 
  */
-public class PlayMenu
-{
-	
-	private Sudoku SudokuBoarda;
-   public PlayMenu(Sudoku SudokuBoard)
-   {
+public class PlayMenu {
+   public PlayMenu(Sudoku SudokuBoard) {
 	  this.SudokuBoarda = SudokuBoard;
    }
    
    public void startPlayMenu(final BackgroundJFrame f) {
-	   
       GridBagLayout gridbag = new GridBagLayout();
       GridBagConstraints c = new GridBagConstraints();
       
-      f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       f.setLayout(gridbag);
       c.fill =GridBagConstraints.BOTH;
-      
-      
+
       JButton playButton = new JButton("Save");
       c.gridx = 0;
       c.gridy = 0;
       c.gridwidth = 3;
+      c.gridheight = 1;
+      
       f.add(playButton, c);
       playButton.addActionListener(new
          ActionListener()
@@ -53,6 +51,10 @@ public class PlayMenu
             {
               //Board resets but need to get the Sudoku grid to reprint
             	SudokuBoarda.resetSudoku();
+            	f.getContentPane().removeAll();
+            	SwingUtilities.updateComponentTreeUI(f);
+            	PlayMenu p = new PlayMenu(SudokuBoarda);
+            	p.startPlayMenu(f);
             }
          });
       
@@ -74,8 +76,8 @@ public class PlayMenu
          });
       
       JButton checkButton = new JButton("Check Answer");
-      c.gridx = 9;
-      c.gridy = 4;
+      c.gridx = 3;
+      c.gridy = 10;
       c.gridwidth =1;
       f.add(checkButton, c);
       checkButton.addActionListener(new
@@ -88,8 +90,8 @@ public class PlayMenu
          });
       
       JButton hintButton = new JButton("Hint");
-      c.gridx = 9;
-      c.gridy = 6;
+      c.gridx = 1;
+      c.gridy = 10;
       f.add(hintButton, c);
       hintButton.addActionListener(new
          ActionListener()
@@ -100,39 +102,49 @@ public class PlayMenu
             }
          });
            
+      SudokuBoard board = new SudokuBoard(SudokuBoarda); 
+      c.gridx = 0;
+      c.gridwidth = 9;
+      c.gridy = 1;
+      c.gridheight = 9;
+      c.weighty = 0.01;
+      c.weightx = 0.01;
+      c.anchor = GridBagConstraints.CENTER;
+      c.insets = new Insets(25,55,25,55);
+      c.fill = GridBagConstraints.BOTH;
+      f.add(board, c);
       
       for(int i = 0; i < 9; i++)
       {
     	  for(int j = 0; j < 9; j++)
     	  {
-    		  JTextField sudokuArea;
+    		  JTextField sudokuArea = board.getSubGrids()[i][j];
+    		  
     		  Font font;
     	      if (SudokuBoarda.getSudoku()[i][j] != 0) {
-    	    	  sudokuArea = new JTextField(Integer.toString(SudokuBoarda.getSudoku()[i][j]));
+    	    	  sudokuArea.setText(Integer.toString(SudokuBoarda.getSudoku()[i][j]));
         	      sudokuArea.setEditable(false);
         	      font = new Font("Verdana", Font.BOLD, 12);
     	      } else {
-    	    	  sudokuArea = new JTextField();
     	    	  sudokuArea.setEditable(true);
     	    	  font = new Font("Verdana", Font.PLAIN, 12);
     	      }
+    	      Document styleDoc = (sudokuArea).getDocument();
+    	      if (styleDoc instanceof AbstractDocument) {
+    	    	  AbstractDocument doc = (AbstractDocument)styleDoc;
+    	    	  doc.setDocumentFilter(new InputLimiter());
+    	      }
     	      sudokuArea.setFont(font);
-    	      sudokuArea.setOpaque(true);
+    	      //sudokuArea.setOpaque(true);
     	      sudokuArea.setHorizontalAlignment(JTextField.CENTER);
-    	      sudokuArea.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-    	      c.gridx = i;
-    	      c.gridy = j+1;
-    	      c.weightx = 1;
-    	      c.weighty = 1;
-    	      c.anchor = GridBagConstraints.CENTER;
-    	      c.insets = new Insets(5,5,5,5);
-    	      f.add(sudokuArea, c);
     	  }
       }
+      
 
       f.setSize(600,500);
       f.setVisible(true);
       
    }
    
+   private Sudoku SudokuBoarda;
 }
