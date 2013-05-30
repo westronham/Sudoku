7c0ac73666187d9a18fddecc515ea9c84e924948
Ron <sacksackley@gmail.com>
2013-05-31 06:24:50 +1000

    Ver 1.8

--- Project/src/SudokuImporter.java
+++ Project/src/SudokuImporter.java
@@ -16,17 +16,17 @@ public class SudokuImporter {
 	public int[] readSudoku(int difficulty) {
 	try { //Get sudoku based on difficulty (test with only 1, it will be "easy". We can replace this with an input class
 		if (difficulty == 1) {
-			File directory = new File("/Users/Ron/Documents/GitHub/Project/Project/Tests/Easy/");      
+			File directory = new File("../Project/Tests/Easy/"); 
 			File[] listOfFiles = directory.listFiles();
 			int randomNum = random.nextInt(listOfFiles.length);
 			scanner = new Scanner(new FileReader(listOfFiles[randomNum]));
 		} else if (difficulty == 2) {
-			File directory = new File("/Users/Ron/Documents/GitHub/Project/Project/Tests/Medium/");      
+			File directory = new File("../Project/Tests/Medium/");      
 			File[] listOfFiles = directory.listFiles();
 			int randomNum = random.nextInt(listOfFiles.length);
 			scanner = new Scanner(new FileReader(listOfFiles[randomNum]));
 		} else if (difficulty == 3) {
-			File directory = new File("/Users/Ron/Documents/GitHub/Project/Project/Tests/Hard/");      
+			File directory = new File("../Project/Tests/Hard/");      
 			File[] listOfFiles = directory.listFiles();
 			int randomNum = random.nextInt(listOfFiles.length);
 			scanner = new Scanner(new FileReader(listOfFiles[randomNum]));
